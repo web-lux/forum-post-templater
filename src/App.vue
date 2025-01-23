d6361025c1134011296d3a9ed3b1d3a8e72bb603
Lux <web-lux@outlook.fr>
2025-01-23 14:06:06 +0100

    wip: html first draft

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const post = ref("");
+const output = computed(() => {
+  return post;
+});
+</script>
 
 <template>
-  <p>Hello World</p>
+  <aside class="character-selection">
+    <button>Bérénice</button>
+    <button>Solomon</button>
+  </aside>
+
+  <main>
+    <div class="input">
+      <div class="quote-field">
+        <label for="quote">Citation</label>
+        <textarea name="quote" id="quote"></textarea>
+      </div>
+
+      <div class="templater">
+        <label for="post">Post RP</label>
+        <div class="templater-input">
+          <div class="templater-buttons">
+            <button>Dialog</button>
+            <button>Asterisk</button>
+            <button>Intermission</button>
+          </div>
+          <textarea name="post" id="post" v-model="post" placeholder="Lorem ispum..."></textarea>
+        </div>
+      </div>
+    </div>
+
+    <div class="output">
+      <label for="result">Output</label>
+      <textarea name="result" id="result" class="result" v-model="output.value" readonly></textarea>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
